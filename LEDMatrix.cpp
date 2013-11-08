b885cf0bb877d6bb0a757eab21270bddf9464aa5
Yihui Xiong <xiongyihui3@gmail.com>
2013-11-08 11:06:53 +0800

    Inital

---
+++ LEDMatrix.cpp
@@ -0,0 +1,176 @@
+/**
+ * LED Matrix library for http://www.seeedstudio.com/depot/ultrathin-16x32-red-led-matrix-panel-p-1582.html
+ * The LED Matrix panel has 32x16 pixels. Several panel can be combined together as a large screen.
+ * 
+ * Coordinate & Connection (Arduino -> panel 0 -> panel 1 -> ...)
+ *   (0, 0)                                     (0, 0)
+ *     +--------+--------+--------+               +--------+--------+
+ *     |   5    |    3   |    1   |               |    1   |    0   |
+ *     |        |        |        |               |        |        |<----- Arduino
+ *     +--------+--------+--------+               +--------+--------+
+ *     |   4    |    2   |    0   |                              (64, 16)
+ *     |        |        |        |<----- Arduino
+ *     +--------+--------+--------+
+ *                             (96, 32)
+ *  Copyright (c) 2013 Seeed Technology Inc.
+ *  @auther     Yihui Xiong
+ *  @date       Nov 8, 2013
+ *  @license    Apache
+ */
+
+#include "LEDMatrix.h"
+#include "Arduino.h"
+
+#if 0
+#define ASSERT(e)   if (!(e)) { Serial.println(#e); while (1); }
+#else
+#define ASSERT(e)
+#endif
+
+LEDMatrix::LEDMatrix(uint8_t a, uint8_t b, uint8_t c, uint8_t d, uint8_t oe, uint8_t r1, uint8_t stb, uint8_t clk)
+{
+    this->clk = clk;
+    this->r1 = r1;
+    this->stb = stb;
+    this->oe = oe;
+    this->a = a;
+    this->b = b;
+    this->c = c;
+    this->d = d;
+
+    mask = 0xff;
+    state = 0;
+}
+
+void LEDMatrix::begin(uint8_t *displaybuf, uint16_t width, uint16_t height)
+{
+    ASSERT(0 == (width % 32));
+    ASSERT(0 == (height % 16));
+
+    this->displaybuf = displaybuf;
+    this->width = width;
+    this->height = height;
+
+    pinMode(a, OUTPUT);
+    pinMode(b, OUTPUT);
+    pinMode(c, OUTPUT);
+    pinMode(d, OUTPUT);
+    pinMode(oe, OUTPUT);
+    pinMode(r1, OUTPUT);
+    pinMode(clk, OUTPUT);
+    pinMode(stb, OUTPUT);
+
+    state = 1;
+}
+
+void LEDMatrix::drawPoint(uint16_t x, uint16_t y, uint8_t pixel)
+{
+    ASSERT(width > x);
+    ASSERT(height > y);
+
+    uint8_t *byte = displaybuf + x * 8 + y / 8;
+    uint8_t  bit = y % 8;
+
+    if (pixel) {
+        *byte |= 0x80 >> bit;
+    } else {
+        *byte &= ~(0x80 >> bit);
+    }
+}
+
+void LEDMatrix::drawRect(uint16_t x1, uint16_t y1, uint16_t x2, uint16_t y2, uint8_t pixel)
+{
+    for (uint16_t x = x1; x < x2; x++) {
+        for (uint16_t y = y1; y < y2; y++) {
+            drawPoint(x, y, pixel);
+        }
+    }
+}
+
+void LEDMatrix::drawImage(uint16_t x1, uint16_t y1, uint16_t x2, uint16_t y2, uint8_t *image)
+{
+    ASSERT(0 == ((x2 - x1) % 8));
+
+    for (uint16_t x = x1; x < x2; x++) {
+        for (uint16_t y = y1; y < y2; y++) {
+            uint8_t *byte = image + x * 8 + y / 8;
+            uint8_t  bit = 7 - (y % 8);
+            uint8_t  pixel = (*byte >> bit) & 1;
+
+            drawPoint(x, y, pixel);
+        }
+    }
+}
+
+void LEDMatrix::clear()
+{
+    uint8_t *ptr = displaybuf;
+    for (uint16_t i = 0; i < (width * height / 8); i++) {
+        *ptr = 0x00;
+        ptr++;
+    }
+}
+
+void LEDMatrix::reverse()
+{
+    mask = ~mask;
+}
+
+uint8_t LEDMatrix::isReversed()
+{
+    return mask;
+}
+
+void LEDMatrix::scan()
+{
+    static uint8_t row = 0;
+
+    if (!state) {
+        return;
+    }
+
+    uint8_t *head = displaybuf + row * (width / 8);
+    for (uint8_t line = 0; line < (height / 16); line++) {
+        uint8_t *ptr = head;
+        head += line * width * 2;
+
+        for (uint8_t byte = 0; byte < (width / 8); byte++) {
+            uint8_t pixels = *ptr;
+            ptr++;
+            pixels = pixels ^ mask;   // reverse: mask = 0xff, normal: mask =0x00 
+            for (uint8_t bit = 0; bit < 8; bit++) {
+                digitalWrite(clk, LOW);
+                digitalWrite(r1, pixels & (0x80 >> bit));
+                digitalWrite(clk, HIGH);
+            }
+        }
+    }
+
+    digitalWrite(oe, HIGH);              // disable display
+
+    // select row
+    digitalWrite(a, (row & 0x01));
+    digitalWrite(b, (row & 0x02));
+    digitalWrite(c, (row & 0x04));
+    digitalWrite(d, (row & 0x08));
+
+    // latch data
+    digitalWrite(stb, LOW);
+    digitalWrite(stb, HIGH);
+
+    digitalWrite(oe, LOW);              // enable display
+
+    row = (row + 1) & 0x0F;
+}
+
+void LEDMatrix::on()
+{
+    state = 1;
+}
+
+void LEDMatrix::off()
+{
+    state = 0;
+    digitalWrite(oe, HIGH);
+}
+
---
+++ LEDMatrix.h
@@ -0,0 +1,89 @@
+/**
+ * LED Matrix library for http://www.seeedstudio.com/depot/ultrathin-16x32-red-led-matrix-panel-p-1582.html
+ * The LED Matrix panel has 32x16 pixels. Several panel can be combined together as a large screen.
+ * 
+ * Coordinate & Connection (Arduino -> panel 0 -> panel 1 -> ...)
+ *   (0, 0)                                     (0, 0)
+ *     +--------+--------+--------+               +--------+--------+
+ *     |   5    |    3   |    1   |               |    1   |    0   |
+ *     |        |        |        |               |        |        |<----- Arduino
+ *     +--------+--------+--------+               +--------+--------+
+ *     |   4    |    2   |    0   |                              (64, 16)
+ *     |        |        |        |<----- Arduino
+ *     +--------+--------+--------+
+ *                             (96, 32)
+ *  Copyright (c) 2013 Seeed Technology Inc.
+ *  @auther     Yihui Xiong
+ *  @date       Nov 7, 2013
+ *  @license    Apache
+ */
+
+
+#ifndef __LED_MATRIX_H__
+#define __LED_MATRIX_H__
+
+ #include <stdint.h>
+
+class LEDMatrix {
+public:
+    LEDMatrix(uint8_t a, uint8_t b, uint8_t c, uint8_t d, uint8_t oe, uint8_t r1, uint8_t stb, uint8_t clk);
+    
+    /**
+     * set the display's display buffer and number, the buffer's size must be not less than 512 * number / 8 bytes
+     * @param displaybuf    display buffer
+     * @param number        panels' number
+     */
+    void begin(uint8_t *displaybuf, uint16_t width, uint16_t height);
+
+    /**
+     * draw a point
+     * @param x     x
+     * @param y     y
+     * @param pixel 0: led off, >0: led on
+     */
+    void drawPoint(uint16_t x, uint16_t y, uint8_t pixel);
+
+    /**
+     * draw a rect
+     * @param (x1, y1)   top-left position
+     * @param (x2, y2)   bottom-right position, not included in the rect
+     * @param pixel      0: rect off, >0: rect on
+     */
+    void drawRect(uint16_t x1, uint16_t y1, uint16_t x2, uint16_t y2, uint8_t pixel);
+
+    /**
+     * draw a image
+     * @param (x1, y1)   top-left position
+     * @param (x2, y2)   bottom-right position, not included in the rect
+     * @param pixels     contents, 1 bit to 1 led
+     */
+    void drawImage(uint16_t x1, uint16_t y1, uint16_t x2, uint16_t y2, uint8_t *image);
+
+    /**
+     * Set screen buffer to zero
+     */
+    void clear();
+
+    /**
+     * turn off 1/16 leds and turn on another 1/16 leds
+     */
+    void scan();
+
+    void reverse();
+
+    uint8_t isReversed();
+
+    void on();
+
+    void off();
+
+private:
+    uint8_t clk, r1, stb, oe, a, b, c, d;
+    uint8_t *displaybuf;
+    uint16_t width;
+    uint16_t height;
+    uint8_t  mask;
+    uint8_t  state;
+};
+
+#endif
